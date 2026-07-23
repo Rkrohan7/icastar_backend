0b343783433e5cffd6f8341d0a5e009acb2fdb16
Rohan Kadam <rohan.kadam@multigenesys.com>
2026-07-23 15:26:39 +0530

    aaded iddue

--- src/main/java/com/icastar/platform/service/JobBulkUploadService.java
+++ src/main/java/com/icastar/platform/service/JobBulkUploadService.java
@@ -13,7 +13,6 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
@@ -55,7 +54,6 @@ public class JobBulkUploadService {
     private static final int COL_START_DATE = 12;
     private static final int COL_RECRUITER_ID = 13;
 
-    @Transactional
     public BulkUploadResponseDto process(MultipartFile file) {
         BulkUploadResponseDto response = BulkUploadResponseDto.builder()
                 .totalRows(0)
